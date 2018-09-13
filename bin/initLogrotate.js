ce732f3a1bf0b12d9a9e05449f871f36ad2dce84
LIUYUAN076 <228474838@qq.com>
2018-09-14 00:14:31 +0800

    修改pm2-logrotate安装脚本

--- bin/initLogrotate.js
+++ bin/initLogrotate.js
@@ -5,6 +5,14 @@ var chalk = require('chalk')
 var pm2LogrotatePath = `${$HOME}/.pm2/modules/pm2-logrotate`
 
 if (!fs.existsSync(pm2LogrotatePath)) {
+  initLogrotate()
+}
+
+if (fs.existsSync(pm2LogrotatePath) && !fs.existsSync(`${pm2LogrotatePath}/node_modules`)) {
+  unInstallLogrotate()
+}
+
+function initLogrotate () {
   console.log(chalk.bold.green('[init] ') + 'module ' + chalk.bold.green('pm2-logrotate'))
   var instalInstance = spawn('npm', ['run', 'i:log'], {
     stdio: 'inherit',
@@ -18,6 +26,24 @@ if (!fs.existsSync(pm2LogrotatePath)) {
   })
 
   instalInstance.on('error', function (err) {
+    console.error(err.stack || err)
+  })
+}
+
+function unInstallLogrotate () {
+  console.log(chalk.bold.red('[error] ') + 'with module ' + chalk.bold.red('pm2-logrotate') + ' reset...')
+  var unInstalInstance = spawn('npm', ['run', ' uni:log'], {
+    stdio: 'inherit',
+    env: process.env,
+    shell: true
+  })
+
+  unInstalInstance.on('close', () => {
+    console.log(chalk.bold.green('[PM2][Module] ') + 'reset successfully')
+    initLogrotate()
+  })
+
+  unInstalInstance.on('error', function (err) {
     console.error(err.stack || err)
   })
 }
